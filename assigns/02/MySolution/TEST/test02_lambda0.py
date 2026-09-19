29e2fa7675331acd064476f4931ee4973311d5d0
RobinPena <144254454+RobinPena@users.noreply.github.com>
2026-09-19 18:36:14 -0400

    added subst0 tests - confirmed subst does not occur in bound variable

--- assigns/02/MySolution/TEST/test02_lambda0.py
+++ assigns/02/MySolution/TEST/test02_lambda0.py
@@ -28,11 +28,12 @@ class TestPair(unittest.TestCase):
         self.assertEqual(t0erm_size(psnd_term), 4)  # 2 for psnd + 2 for pair
 
 class TestPairFvset(unittest.TestCase):
-    
+    #the free variables of a pair is the union of free variables of its components
     def test_pair_fvset(self):
         pair_term = T0Mpair(T0Mvar("x"), T0Mvar("y"))
-        self.assertEqual(t0erm_fvset(pair_term), {"x", "y"})
+        self.assertEqual(t0erm_fvset(pair_term), {"x", "y"}) 
 
+    #free variables of projections should be the same as the free variables of the operand    
     def test_projections_fvset(self):
         pair_term = T0Mpair(T0Mvar("x"), T0Mvar("y"))
         pfst_term = T0Mpfst(pair_term)
@@ -40,6 +41,31 @@ class TestPairFvset(unittest.TestCase):
         self.assertEqual(t0erm_fvset(pfst_term), {"x", "y"})
         self.assertEqual(t0erm_fvset(psnd_term), {"x", "y"})
 
+class TestPairSubstitution(unittest.TestCase):
+    #substituting in a pair should substitute in both components
+    def test_pair_substitution(self): 
+        pair_term = T0Mpair(T0Mvar("x"), T0Mvar("y"))
+        substituted_term = t0erm_subst0(pair_term, "x", T0Mint(42))
+        expected_term = T0Mpair(T0Mint(42), T0Mvar("y"))
+        self.assertEqual(substituted_term, expected_term)
 
+#substituting in projections substitutes the operand of the projection
+    def test_projections_substitution(self):
+        pair_term = T0Mpair(T0Mvar("x"), T0Mvar("y"))
+        pfst_term = T0Mpfst(pair_term)
+        psnd_term = T0Mpsnd(pair_term)
+        substituted_pfst = t0erm_subst0(pfst_term, "x", T0Mint(55))
+        substituted_psnd = t0erm_subst0(psnd_term, "y", T0Mint(12))
+        expected_pfst = T0Mpfst(T0Mpair(T0Mint(55), T0Mvar("y")))
+        expected_psnd = T0Mpsnd(T0Mpair(T0Mvar("x"), T0Mint(12)))
+        self.assertEqual(substituted_pfst, expected_pfst)
+        self.assertEqual(substituted_psnd, expected_psnd)
+
+    def test_binder_substitution(self):
+        # Test that substitution does not occur in the bound variable of a lambda
+        lambda_term = T0Mlam("x", T0Mpair(T0Mvar("x"), T0Mvar("y")))
+        substituted_term = t0erm_subst0(lambda_term, "x", T0Mint(55))
+        expected_term = T0Mlam("x", T0Mpair(T0Mvar("x"), T0Mvar("y")))  # No substitution should occur
+        self.assertEqual(substituted_term, expected_term)
 if __name__ == "__main__":
     unittest.main()
